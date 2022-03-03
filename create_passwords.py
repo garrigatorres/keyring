8a4d82d82475ead520a2561ea958a12a5eb03a86
garrigatorres <josep.garriga@escolaarrels.com>
2022-03-03 19:30:39 +0100

    Update and rename crear_passwords.py to create_passwords.py

--- create_passwords.py
+++ create_passwords.py
@@ -4,12 +4,12 @@ import keyring
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!£$%^&*(`)"
 password_len = 8
 
-f = open("usuaris", "r")
+f = open("users", "r")
 for x in f:
   password  = ""
   for y in range(0,password_len):
     password_char = random.choice(chars)
     password = password + password_char
-  keyring.set_password("keyring_nou", x.rstrip(), password)
+  keyring.set_password("new_keyring", x.rstrip(), password)
 
 f.close()
